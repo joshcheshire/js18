73582f288e67ba39b8c5bd4090c02f3bd36e56c1
Student <student@RU-017.local>
2015-09-25 12:08:52 -0600

    Randoms working

--- main.js
+++ main.js
@@ -11,10 +11,56 @@ var mainControllerFunc = function($scope){
 
 $scope.quotes = []
 
+$scope.showPopup = false
+// $scope.myArray = ['Quote 1', 'Quote 2', 'Quote 3', 'Quote 4', 'Quote 5']
 
-$scope.sortRating = function(){
+
+
+
+
+$scope.randomQuotes = [
+	{
+		quote  : 'This is the fucking shit',
+		author : 'Alex'
+	},
+
+	{   quote: 'Thanks to you',
+		author: 'Me'
+	},
+	{	quote: 'Random ass quote',
+		author: 'Larry'
+	} 
+
+]
 
 	
+
+
+
+
+
+$scope.randomQuote = ''
+
+
+$scope.getRandomQuote = function(event){
+
+	$scope.showPopup = !$scope.showPopup;
+	console.log("random getter")
+
+	
+
+	 $scope.randomQ = function(){
+
+	 	// $scope.randomQuotes.Math.floor(Math.random() * $scope.randomQuotes.length);
+		
+		// $scope.randomQuotesArray.push($scope.randomQuotes)
+		// $scope.randomQuote = $scope.quotes[0]
+		// accessing the quotes array is your issue
+		$scope.randomQuote = $scope.randomQuotes[Math.floor(Math.random() * $scope.randomQuotes.length)]
+	}()
+		return ($scope.randomQuote)
+
+
 }
 
 
@@ -37,3 +83,7 @@ if ($scope.newScript.quote != '' && $scope.newScript.author != ''){
 angular.module('myApp').controller('mainController' , ['$scope', mainControllerFunc])
 
 
+// var 
+
+// var randomQ = myArray[Math.floor(Math.random() * myArray.length)];
+// console.log(randomQ)
